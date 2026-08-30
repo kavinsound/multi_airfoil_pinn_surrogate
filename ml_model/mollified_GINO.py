bf6653654d8759292514e2a7fe00ea9b39b276bd
Kavin Soundirarajan <kavinsoundirarajan@gmail.com>
2026-08-30 16:25:52 -0400

    updated hyperparam

--- ml_model/mollified_GINO.py
+++ ml_model/mollified_GINO.py
@@ -7,7 +7,8 @@ model = GINO(
     out_channels=9,
     hidden_channels=32,
     in_gno_radius=0.1,
+    gno_coord_dim=2,
     gno_weighting_function='bump',
-    fno_n_modes=(16, 16),
+    fno_n_modes=(32, 32),
     fno_n_layers=4,
 )
